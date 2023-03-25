58a399c0a1d19d14928763de3084a13164ddd27e
alanbonoft <alan.bonoft@gmail.com>
2023-03-25 12:36:39 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.NetworkInformation;
+
+namespace firstconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Dictionary<string, string> nics = new();
+
+                foreach(NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    Console.WriteLine(ni.Name);
+                    foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            Console.WriteLine(ip.Address.ToString());
+                            nics.Add(ni.Name, ip.Address.ToString());
+                        }
+                    }
+                } 
+                
+                var name = "eth0";
+                Console.WriteLine($"{name} is at {nics[name]}");
+                name = "wlan0";
+                Console.WriteLine($"{name} is at {nics[name]}");
+                name = "wlan";
+                Console.WriteLine($"{name} is at {nics[name]}");
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
